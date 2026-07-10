3cb8c87f18841801a39fad8640f40d3616247288
Kartik Verma <kartikverma2204number1@gmail.com>
2026-07-10 23:16:31 +0530

    did largest reactangle in hostogram question ques no 84 hard

--- hard/largestrectangleinhistogram.java
+++ hard/largestrectangleinhistogram.java
@@ -21,43 +21,90 @@
 // 1 <= heights.length <= 105
 // 0 <= heights[i] <= 104
 
+// class Solution {
+//     public static int largestRectangleArea(int[] heights) {
+//         if (heights.length == 1)
+//             return heights[0];
+//         if (heights.length == 2)
+//             return Math.max(2 * Math.min(heights[0], heights[1]), Math.max(heights[0], heights[1]));
+
+//         boolean onlyzeros = true;
+//         for (int i = 0; i < heights.length - 1; i++) {
+//             if (heights[i] != 0) {
+//                 onlyzeros = false;
+//                 break;
+//             }
+//         }
+//         if (onlyzeros)
+//             return 0;
+//         int minht = Integer.MAX_VALUE;
+//         int maxsum = Integer.MIN_VALUE;
+//         int sum = -1;
+//         // int[] arr = new int[heights.length];
+//         for (int i = 0; i < heights.length - 1; i++) {
+//             minht = heights[i];
+//             sum = heights[i];
+//             maxsum = Math.max(maxsum, sum);
+//             for (int j = i + 1; j < heights.length; j++) {
+//                 minht = Math.min(minht, Math.min(heights[i], heights[j]));
+//                 sum = minht * (j - i + 1);
+//                 maxsum = Math.max(maxsum, sum);
+//             }
+//         }
+//         maxsum = Math.max(maxsum,heights[heights.length-1]);
+//         return maxsum;
+//     }
+
+//     public static void main(String[] args) {
+//         int[] heights = { 2, 1, 5, 6, 2, 3 };
+
+//         System.out.println(largestRectangleArea(heights));
+//     }
+// }
+import java.util.*;
+
 class Solution {
     public static int largestRectangleArea(int[] heights) {
-        if (heights.length == 1)
-            return heights[0];
-        if (heights.length == 2)
-            return Math.max(2 * Math.min(heights[0], heights[1]), Math.max(heights[0], heights[1]));
+        int[] rs = new int[heights.length];
+        int[] ls = new int[heights.length];
+        Stack<Integer> s = new Stack<>();
+        int maxarea = 0;
 
-        boolean onlyzeros = true;
-        for (int i = 0; i < heights.length - 1; i++) {
-            if (heights[i] != 0) {
-                onlyzeros = false;
-                break;
-            }
+        for (int i = heights.length - 1; i >= 0; i--) {
+            while (s.size() > 0 && heights[i] <= heights[s.peek()])
+                s.pop();
+            rs[i] = s.empty() ? heights.length : s.peek();
+            s.push(i);
         }
-        if (onlyzeros)
-            return 0;
-        int minht = Integer.MAX_VALUE;
-        int maxsum = Integer.MIN_VALUE;
-        int sum = -1;
-        // int[] arr = new int[heights.length];
-        for (int i = 0; i < heights.length - 1; i++) {
-            minht = heights[i];
-            sum = heights[i];
-            maxsum = Math.max(maxsum, sum);
-            for (int j = i + 1; j < heights.length; j++) {
-                minht = Math.min(minht, Math.min(heights[i], heights[j]));
-                sum = minht * (j - i + 1);
-                maxsum = Math.max(maxsum, sum);
-            }
+
+        while (!s.empty())
+            s.pop();
+
+        for (int i = 0; i < heights.length; i++) {
+            while (s.size() > 0 && heights[i] <= heights[s.peek()])
+                s.pop();
+            ls[i] = s.empty() ? -1 : s.peek();
+            s.push(i);
         }
-        maxsum = Math.max(maxsum,heights[heights.length-1]);
-        return maxsum;
+
+        for (int i = 0; i < heights.length; i++) {
+            int area = heights[i] * (rs[i] - ls[i] - 1);
+            maxarea = Math.max(maxarea, area);
+        }
+
+        return maxarea;
     }
 
     public static void main(String[] args) {
-        int[] heights = { 2, 1, 5, 6, 2, 3 };
-
+        // int[] heights = {2,1,5,6,2,3};
+        int[] heights = { 2, 4 };
         System.out.println(largestRectangleArea(heights));
+    }
+
+    public static void printArray(int[] arr) {
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
     }
 }
