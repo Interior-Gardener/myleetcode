5e3faa7f8d0815d4c7037e41d4b7153cd3786221
Kartik Verma <kartikverma2204number1@gmail.com>
2026-08-10 20:23:59 +0530

    Failed attempt of rotate list by k position reason too slow ques 61

---
+++ medium/rotatelist.java
@@ -0,0 +1,77 @@
+
+// Code
+// 61. Rotate List
+// Attempted
+// Medium
+// Topics
+// premium lock icon
+// Companies
+// Given the head of a linked list, rotate the list to the right by k places.
+
+// Example 1:
+
+// Input: head = [1,2,3,4,5], k = 2
+// Output: [4,5,1,2,3]
+// Example 2:
+
+// Input: head = [0,1,2], k = 4
+// Output: [2,0,1]
+
+// Constraints:
+
+// The number of nodes in the list is in the range [0, 500].
+// -100 <= Node.val <= 100
+// 0 <= k <= 2 * 109
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public ListNode rotateRight(ListNode head, int k) {
+        if (head == null || head.next == null)
+            return head;
+        ListNode newhead = helper(head, k);
+        return newhead;
+    }
+
+    public ListNode helper(ListNode head, int k) {
+        while (k != 0) {
+            ListNode oldhead = head;
+            ListNode prev = null;
+            while (head.next != null) {
+                if (head.next.next == null) {
+                    prev = head;
+                }
+                head = head.next;
+            }
+            prev.next = null;
+            head.next = oldhead;
+            k--;
+        }
+        return head;
+    }
+}
