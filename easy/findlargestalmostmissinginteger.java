5f33eb36cf9df8745461df69e9fd2a9f28176802
Kartik Verma <kartikverma2204number1@gmail.com>
2026-08-18 22:18:07 +0530

    Aaj ka POTD find largest almost missing element ques 3471

---
+++ easy/findlargestalmostmissinginteger.java
@@ -0,0 +1,105 @@
+// 3471. Find the Largest Almost Missing Integer
+// Easy
+// Topics
+// premium lock icon
+// Companies
+// Hint
+// You are given an integer array nums and an integer k.
+
+// An integer x is almost missing from nums if x appears in exactly one subarray of size k within nums.
+
+// Return the largest almost missing integer from nums. If no such integer exists, return -1.
+
+// A subarray is a contiguous sequence of elements within an array.
+ 
+
+// Example 1:
+
+// Input: nums = [3,9,2,1,7], k = 3
+
+// Output: 7
+
+// Explanation:
+
+// 1 appears in 2 subarrays of size 3: [9, 2, 1] and [2, 1, 7].
+// 2 appears in 3 subarrays of size 3: [3, 9, 2], [9, 2, 1], [2, 1, 7].
+// 3 appears in 1 subarray of size 3: [3, 9, 2].
+// 7 appears in 1 subarray of size 3: [2, 1, 7].
+// 9 appears in 2 subarrays of size 3: [3, 9, 2], and [9, 2, 1].
+// We return 7 since it is the largest integer that appears in exactly one subarray of size k.
+
+// Example 2:
+
+// Input: nums = [3,9,7,2,1,7], k = 4
+
+// Output: 3
+
+// Explanation:
+
+// 1 appears in 2 subarrays of size 4: [9, 7, 2, 1], [7, 2, 1, 7].
+// 2 appears in 3 subarrays of size 4: [3, 9, 7, 2], [9, 7, 2, 1], [7, 2, 1, 7].
+// 3 appears in 1 subarray of size 4: [3, 9, 7, 2].
+// 7 appears in 3 subarrays of size 4: [3, 9, 7, 2], [9, 7, 2, 1], [7, 2, 1, 7].
+// 9 appears in 2 subarrays of size 4: [3, 9, 7, 2], [9, 7, 2, 1].
+// We return 3 since it is the largest and only integer that appears in exactly one subarray of size k.
+
+// Example 3:
+
+// Input: nums = [0,0], k = 1
+
+// Output: -1
+
+// Explanation:
+
+// There is no integer that appears in only one subarray of size 1.
+
+ 
+
+// Constraints:
+
+// 1 <= nums.length <= 50
+// 0 <= nums[i] <= 50
+// 1 <= k <= nums.length
+
+class Solution {
+    public int largestInteger(int[] nums, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int n = nums.length;
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+        }
+        if (k == 1) {
+            int max = -1;
+            for (int i : map.keySet()) {
+                if (map.get(i) == 1 && i > max) {
+                    max = i;
+                }
+            }
+            if (max == -1)
+                return -1;
+            return max;
+        } else if (k == n) {
+            int max = -1;
+            for (int i : map.keySet()) {
+                if (i > max) {
+                    max = i;
+                }
+            }
+            if (max == -1)
+                return -1;
+            return max;
+        } else {
+            if (map.get(nums[0]) == 1 && map.get(nums[n - 1]) != 1)
+                return nums[0];
+            else if (map.get(nums[0]) != 1 && map.get(nums[n - 1]) == 1)
+                return nums[n - 1];
+            else if (map.get(nums[0]) == 1 && map.get(nums[n - 1]) == 1) {
+                if (nums[0] > nums[n - 1])
+                    return nums[0];
+                return nums[n - 1];
+            }
+            return -1;
+
+        }
+    }
+}
