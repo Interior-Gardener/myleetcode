9adee5d43d0faa9edac578813bed179967065135
Kartik Verma <kartikverma2204number1@gmail.com>
2026-09-09 18:29:10 +0530

    Aaj ka POTD Solved count commas in range 2 ques 3871

---
+++ medium/Countcommasinrange2.java
@@ -0,0 +1,69 @@
+// 3871. Count Commas in Range II
+// Medium
+// Topics
+// premium lock icon
+// Companies
+// Hint
+// You are given an integer n.
+
+// Return the total number of commas used when writing all integers from [1, n] (inclusive) in standard number formatting.
+
+// In standard formatting:
+
+// A comma is inserted after every three digits from the right.
+// Numbers with fewer than 4 digits contain no commas.
+ 
+
+// Example 1:
+
+// Input: n = 1002
+
+// Output: 3
+
+// Explanation:
+
+// The numbers "1,000", "1,001", and "1,002" each contain one comma, giving a total of 3.
+
+// Example 2:
+
+// Input: n = 998
+
+// Output: 0
+
+// Explanation:
+
+// ​​​​​​​All numbers from 1 to 998 have fewer than four digits. Therefore, no commas are used.
+
+ 
+
+// Constraints:
+
+// 1 <= n <= 1015
+
+class Solution {
+
+    public long countCommas(long n) {
+        long res = 0;
+
+        if (n < 1000) return 0;
+
+        res += n - 999;
+
+        if (n < 1_000_000) return res;
+
+        res += (n - 999_999);
+
+        if (n < 1_000_000_000) return res;
+
+        res += (n - 999_999_999);
+
+        if (n < 1_000_000_000_000L) return res;
+
+        res += (n - 999_999_999_999L);
+
+        if(n == 1_000_000_000_000_000L)
+        return res+1;
+
+        return res;
+    }
+}
