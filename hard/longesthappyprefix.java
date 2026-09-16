ad1c85dcd5cc64bc146c36d65c005b6b80088799
Kartik Verma <kartikverma2204number1@gmail.com>
2026-09-16 17:59:01 +0530

    Solved longest happy prefix which is a prt of KMP algorithm ques 1392

---
+++ hard/longesthappyprefix.java
@@ -0,0 +1,55 @@
+// 1392. Longest Happy Prefix
+// Hard
+// Topics
+// premium lock icon
+// Companies
+// Hint
+// A string is called a happy prefix if it is a non-empty prefix which is also a suffix (excluding itself).
+
+// Given a string s, return the longest happy prefix of s. Return an empty string "" if no such prefix exists.
+
+ 
+
+// Example 1:
+
+// Input: s = "level"
+// Output: "l"
+// Explanation: s contains 4 prefix excluding itself ("l", "le", "lev", "leve"), and suffix ("l", "el", "vel", "evel"). The largest prefix which is also suffix is given by "l".
+// Example 2:
+
+// Input: s = "ababab"
+// Output: "abab"
+// Explanation: "abab" is the largest prefix which is also suffix. They can overlap in the original string.
+ 
+
+// Constraints:
+
+// 1 <= s.length <= 105
+// s contains only lowercase English letters.
+
+class Solution {
+    public String longestPrefix(String s) {
+        String res;
+        int n = s.length();
+        int[] lps = new int[n];
+        int len = 0;
+        int i = 1;
+        while (i < n) {
+            if (s.charAt(i) == s.charAt(len)) {
+                len++;
+                lps[i] = len;
+                i++;
+            } else {
+                if (len > 0) {
+                    len = lps[len - 1];
+                } else {
+                    lps[i] = 0;
+                    i++;
+                }
+            }
+        }
+
+        res = s.substring(0, lps[n - 1]);
+        return res;
+    }
+}
