0776206fbe7bd7056fabf9c968d31221be8a4fc4
Kartik Verma <kartikverma2204number1@gmail.com>
2026-09-21 16:46:45 +0530

    Did count digit appearances ques 3895

---
+++ medium/countdigitappearances.java
@@ -0,0 +1,57 @@
+// 3895. Count Digit Appearances
+// Medium
+// Topics
+// premium lock icon
+// Companies
+// Hint
+// You are given an integer array nums and an integer digit.
+
+// Return the total number of times digit appears in the decimal representation of all elements in nums.
+
+ 
+
+// Example 1:
+
+// Input: nums = [12,54,32,22], digit = 2
+
+// Output: 4
+
+// Explanation:
+
+// The digit 2 appears once in 12 and 32, and twice in 22. Thus, the total number of times digit 2 appears is 4.
+
+// Example 2:
+
+// Input: nums = [1,34,7], digit = 9
+
+// Output: 0
+
+// Explanation:
+
+// The digit 9 does not appear in the decimal representation of any element in nums, so the total number of times digit 9 appears is 0.
+
+ 
+
+// Constraints:
+
+// 1 <= nums.length <= 1000
+// 1 <= nums[i] <= 106​​​​​​​
+// 0 <= digit <= 9
+
+class Solution {
+    int res = 0;
+
+    public int countDigitOccurrences(int[] nums, int k) {
+        for (int i : nums) {
+            countdigit(i,k);
+        }
+        return res;
+    }
+
+    void countdigit(int i , int k) {
+        while(i > 0) {
+            if(i%10 == k) res++;
+            i/=10;
+        }
+    }
+}
