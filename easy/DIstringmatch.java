d81242c95e04e0d85f4ff428479b0d4d37aea3ee
Kartik Verma <kartikverma2204number1@gmail.com>
2026-09-22 19:18:06 +0530

    Solved DI String match ques 942

---
+++ easy/DIstringmatch.java
@@ -0,0 +1,49 @@
+// 942. DI String Match
+// Easy
+// Topics
+// premium lock icon
+// Companies
+// A permutation perm of n + 1 integers of all the integers in the range [0, n] can be represented as a string s of length n where:
+
+// s[i] == 'I' if perm[i] < perm[i + 1], and
+// s[i] == 'D' if perm[i] > perm[i + 1].
+// Given a string s, reconstruct the permutation perm and return it. If there are multiple valid permutations perm, return any of them.
+
+// Example 1:
+
+// Input: s = "IDID"
+// Output: [0,4,1,3,2]
+// Example 2:
+
+// Input: s = "III"
+// Output: [0,1,2,3]
+// Example 3:
+
+// Input: s = "DDI"
+// Output: [3,2,0,1]
+
+// Constraints:
+
+// 1 <= s.length <= 105
+// s[i] is either 'I' or 'D'.
+
+import java.util.*;
+
+class Solution {
+    public int[] diStringMatch(String s) {
+        int n = s.length();
+        int st = 0;
+        int[] res = new int[s.length() + 1];
+        for (int i = 0; i < res.length - 1; i++) {
+            if (s.charAt(i) == 'I') {
+                res[i] = st;
+                st++;
+            } else {
+                res[i] = n;
+                n--;
+            }
+        }
+        res[res.length - 1] = (st + (n - st) / 2);
+        return res;
+    }
+}
