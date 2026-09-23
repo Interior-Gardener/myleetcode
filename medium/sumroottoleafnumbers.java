5b46d8aba9189a7bd41098a4d243f805e32d7ed5
Kartik Verma <kartikverma2204number1@gmail.com>
2026-09-23 15:20:23 +0530

    Did number of 1 bits ques 191

--- medium/sumroottoleafnumbers.java
+++ medium/sumroottoleafnumbers.java
@@ -12,10 +12,7 @@
 
 // A leaf node is a node with no children.
 
- 
-
 // Example 1:
-
 
 // Input: root = [1,2,3]
 // Output: 25
@@ -25,7 +22,6 @@
 // Therefore, sum = 12 + 13 = 25.
 // Example 2:
 
-
 // Input: root = [4,9,0,5,1]
 // Output: 1026
 // Explanation:
@@ -33,7 +29,6 @@
 // The root-to-leaf path 4->9->1 represents the number 491.
 // The root-to-leaf path 4->0 represents the number 40.
 // Therefore, sum = 495 + 491 + 40 = 1026.
- 
 
 // Constraints:
 
@@ -46,19 +41,46 @@ import java.util.*;
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
  * }
  */
 class Solution {
+    public class TreeNode {
+
+        int val;
+
+        TreeNode left;
+
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+
+            this.val = val;
+
+            this.left = left;
+
+            this.right = right;
+
+        }
+
+    }
+
     int res = 0;
 
     public int sumNumbers(TreeNode root) {
