6035ec056ae411e139b1d90c383b655b9ad9b9dc
Kartik Verma <kartikverma2204number1@gmail.com>
2026-09-24 20:05:30 +0530

    Started learning TRIE data structure implemented Trie or prefix tree ques 208

---
+++ medium/ImplementTrieorPrefixtree.java
@@ -0,0 +1,120 @@
+// 208. Implement Trie (Prefix Tree)
+// Medium
+// Topics
+// premium lock icon
+// Companies
+// A trie (pronounced as "try") or prefix tree is a tree data structure used to efficiently store and retrieve keys in a dataset of strings. There are various applications of this data structure, such as autocomplete and spellchecker.
+
+// Implement the Trie class:
+
+// Trie() Initializes the trie object.
+// void insert(String word) Inserts the string word into the trie.
+// boolean search(String word) Returns true if the string word is in the trie (i.e., was inserted before), and false otherwise.
+// boolean startsWith(String prefix) Returns true if there is a previously inserted string word that has the prefix prefix, and false otherwise.
+ 
+
+// Example 1:
+
+// Input
+// ["Trie", "insert", "search", "search", "startsWith", "insert", "search"]
+// [[], ["apple"], ["apple"], ["app"], ["app"], ["app"], ["app"]]
+// Output
+// [null, null, true, false, true, null, true]
+
+// Explanation
+// Trie trie = new Trie();
+// trie.insert("apple");
+// trie.search("apple");   // return True
+// trie.search("app");     // return False
+// trie.startsWith("app"); // return True
+// trie.insert("app");
+// trie.search("app");     // return True
+ 
+
+// Constraints:
+
+// 1 <= word.length, prefix.length <= 2000
+// word and prefix consist only of lowercase English letters.
+// At most 3 * 104 calls in total will be made to insert, search, and startsWith.
+
+import java.util.*;
+
+class Trie {
+    class TrieNode {
+        TrieNode[] children;
+        boolean eow;
+
+        public TrieNode() {
+            children = new TrieNode[26];
+            for (int i = 0; i < 26; i++) {
+                children[i] = null;
+            }
+            eow = false;
+        }
+    }
+
+    TrieNode root;
+
+    public Trie() {
+        root = new TrieNode();
+    }
+
+    public void insert(String word) {
+        TrieNode curr = root;
+        for (int i = 0; i < word.length(); i++) {
+            int idx = word.charAt(i) - 'a';
+
+            if (curr.children[idx] == null) {
+                curr.children[idx] = new TrieNode();
+            }
+
+            if (i == word.length() - 1) {
+                curr.children[idx].eow = true;
+            }
+
+            curr = curr.children[idx];
+        }
+    }
+
+    public boolean search(String key) {
+        TrieNode curr = root;
+
+        for (int i = 0; i < key.length(); i++) {
+            int idx = key.charAt(i) - 'a';
+
+            if (curr.children[idx] == null) {
+                return false;
+            }
+
+            if (i == key.length() - 1 && curr.children[idx].eow == false) {
+                return false;
+            }
+
+            curr = curr.children[idx];
+        }
+        return true;
+    }
+
+    public boolean startsWith(String prefix) {
+        TrieNode curr = root;
+
+        for (int i = 0; i < prefix.length(); i++) {
+            int idx = prefix.charAt(i) - 'a';
+
+            if (curr.children[idx] == null) {
+                return false;
+            }
+
+            curr = curr.children[idx];
+        }
+        return true;
+    }
+}
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie obj = new Trie();
+ * obj.insert(word);
+ * boolean param_2 = obj.search(word);
+ * boolean param_3 = obj.startsWith(prefix);
+ */
