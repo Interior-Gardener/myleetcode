1908d2978a76ecbc705e9bc36257ecada3ffdb98
Kartik Verma <kartikverma2204number1@gmail.com>
2026-09-24 19:00:05 +0530

    Did smallest index with digit sum equal to index ques 3550

--- easy/numberof1bits.java
+++ easy/numberof1bits.java
@@ -5,8 +5,6 @@
 // Companies
 // Given a positive integer n, write a function that returns the number of set bits in its binary representation (also known as the Hamming weight).
 
- 
-
 // Example 1:
 
 // Input: n = 11
